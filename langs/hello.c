5c362ee88cb8e3c80ab673cb7a35a115ad76a75a
Andy Harris <andy@aharrisbooks.net>
2025-01-10 16:25:05 +0000

    messed aroiund with C and Java versions

--- langs/hello.c
+++ langs/hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(){
-  char userName[20];
+  // this is a comment
+  	char userName[20];
   printf("What is your name? ");
   scanf("%s", userName);
   printf("Hi %s from C \n", userName);
