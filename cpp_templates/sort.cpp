8575d40685a4971da8b506635b0c2a4d37506b43
Andy Harris <andy@aharrisbooks.net>
2025-02-19 13:20:10 +0000

    add vector examples to shuffle in cpp

--- cpp_templates/sort.cpp
+++ cpp_templates/sort.cpp
@@ -1,7 +1,9 @@
 #include <algorithm>
 #include <iostream>
+#include <vector>
 
 void printArray();
+void printVec(std::vector<int>& v);
 
 int A[] = {1, 4, 2, 8, 5, 7};
 const int N = sizeof(A) / sizeof(int);
@@ -10,12 +12,24 @@ int main(){
          
 	printArray();
 
-
 	std::random_shuffle(A, A + N);
 	printArray();
 
 	std::sort(A, A + N);
 	printArray();
+
+	std::vector<int> v;
+	v.push_back(1);
+	v.push_back(4);
+	v.push_back(2);
+	v.push_back(8);
+	v.push_back(5);
+	v.push_back(7);
+
+	std::random_shuffle(v.begin(), v.end());
+        printVec(v);
+	std::sort(v.begin(), v.end());
+	printVec(v);
 
   	return(0);
 
@@ -27,3 +41,12 @@ void printArray(){
 	} // end for
         std::cout << std::endl << std::endl; 
 } // end printArray
+
+void printVec(std::vector<int>& v){
+  for (int i: v){
+    std::cout << i << ", ";
+  } // end for
+  std::cout << std::endl;
+} // end printVector()
+  
+
